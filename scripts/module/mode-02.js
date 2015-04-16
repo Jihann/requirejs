823afedce612adf7199c5b0db9a9b3a002f7c973
Jihann <1244098192@qq.com>
2015-04-16 16:04:21 +0800

    添加注释

--- scripts/module/mode-02.js
+++ scripts/module/mode-02.js
@@ -3,7 +3,10 @@
  * 采用require.js模块化编程
  */
 
-//采用require.js模块编程
+/*
+  采用require.js模块编程
+  要使用jquery对象，则该模块依赖于jquery
+ */
 define(["jquery"], function(){
    function show(){
        alert("Hello World");
